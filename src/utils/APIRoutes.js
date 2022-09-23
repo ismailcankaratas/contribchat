04bdad8826ff24bef16b855cc68f39cf74e91cfc
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-09-23 19:28:34 +0300

    bug fix

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000"; // https://mylittlecode-api.herokuapp.com
+export const host = "https://contribchat.vercel.app/"; // https://contribchat.vercel.app/
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
